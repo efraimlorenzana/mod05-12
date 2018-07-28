15e003b5cc45c0c2c1c7853533d05aad7449fe7a
Efraim A. Lorenzana <efraim.lorenzana@ddtkonstract.com>
2018-07-28 18:16:52 +0800

    re-analize, re-structured and clean the code : completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,87 @@
 let txtNum = document.querySelector('#txtNum');
 let txtWord = document.querySelector('#txtWord');
 
-let ones = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'];
+let ones = ['','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'];
 let teen = ['Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen', 'Eighteen', 'Nineteen', 'Twenty'];
 let tens = ['','','Twenty','Thirty','Fourty','Fifty','Sixty','Seventy','Eighty','Ninety'];
-let bigNumber = ['','','','Hundred','Thousand','Million','Billion'];
+let bigNumber = ['','','Thousand','Million','Billion','Trillion','Quadrillion'];
 
 const convertNumToWord = () => {
-	let numLen = txtNum.value.length;
-	let convertedWord = "";
-	switch (numLen){
-		case 1: convertedWord = ones[txtNum.value]; break;
-		case 2: convertedWord = getTens(txtNum.value); break;
-		case 3: convertedWord = getHundred(numLen,txtNum.value); break;
-		case 4: 
-				let numHundred = txtNum.value.slice(-3);
-				convertedWord = getHundred(3,numHundred);
-			break;
-		default: break;
+	let places = 1;
+	let result = "";
+	let cent = getCents(txtNum.value);
+	let Num = txtNum.value.split('.')[0];
+	let temp = "";
+	let pesos = "Pesos";
+
+	while(Num != 0) {
+		temp = getHundred(Num.slice(-3));
+		result = `${temp} ${bigNumber[places]} ${result}`;
+
+		if(Num.length > 3)
+			Num = Num.substring(0,(Num.length - 3));
+		else
+			Num = 0;
+
+		places++;
 	}
-	txtWord.textContent = convertedWord;
+
+	if(result.trim() === "One"){
+		pesos = 'peso';
+	}
+
+	txtWord.textContent = `${result} ${pesos} ${cent}`;
 }
 
-getTens = (num) => {
-	if(num[0] === "1") {
-		convertedWord = teen[num[1]];
+const getCents = (Num) => {
+	let dec = Num.split('.');
+	let result = "";
+	let cent = "Centavos";
+
+	if(dec[1] != undefined){
+		if(dec[1] == 1)
+			cent = "Centavo";
+
+		if(dec[1].length == 1)
+			dec[1] = dec[1] + "0";
+
+		result = ` and ${getHundred(dec[1])} ${cent}`;
+	}
+
+	return result;
+}
+
+const getHundred = (Num) => {
+	let strNum = "000" + Num;
+	let newNum = strNum.slice(-3);
+	let result = "";
+
+	if(newNum[0] != 0)
+		result = `${ones[newNum[0]]} Hundred `;
+
+	if(newNum[1] == 0)
+		result += ones[newNum[2]];
+	else
+		result += getTens(newNum);
+
+	return result;
+}
+
+const getTens = (Num) => {
+	let tensNum = Num.slice(-2);
+	let result = "";
+
+
+	if(tensNum[0] == 1) {
+		result = teen[tensNum[1]];
 	} else {
-		if(num[1] === "0"){
-			convertedWord = tens[num[0]];
-		} else {
-			convertedWord = tens[num[0]] + "-" + ones[num[1]];
-		}
-	}
-	return convertedWord;
-}
-
-getHundred = (numLen,num) => {
-	if(num[1] === "0" && num[2] === "0") {
-				/// exact hundreds
-		convertedWord = ones[num[0]] + " " + bigNumber[numLen];
-	} else if(num[1] === "0" && num[2] !== "0") {
-		convertedWord = ones[num[0]] + " " + bigNumber[numLen] + " " + ones[num[2]];
-	} else if(num[1] === "1") {
-		convertedWord = ones[num[0]] + " " + bigNumber[numLen] + " " + teen[num[2]];
-	} else if(num[1] !== "0" && num[2] !== "0") {
-		convertedWord = ones[num[0]] + " " + bigNumber[numLen] + " " + tens[num[1]] + "-" + ones[num[2]];
-	} else if(num[1] !== "0" && num[2] === "0") {
-		convertedWord = ones[num[0]] + " " + bigNumber[numLen] + " " + tens[num[1]];
+		result = tens[tensNum[0]] + ` ${ones[tensNum[1]]}`;
 	}
 
-	return convertedWord;
+
+	if(tensNum.length == 1)
+		result = ones[tensNum];
+
+	return result;
 }
+
